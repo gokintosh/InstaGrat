31b71e5cc8538edf548bc000dca61f2f8aa329fd
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2025-06-29 01:59:13 +0200

    add create user method

---
+++ src/main/java/me/gokulnair/instagrat/InstagratApplication.java
@@ -0,0 +1,13 @@
+package me.gokulnair.instagrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagratApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/gokulnair/instagrat/controller/UserController.java
@@ -0,0 +1,26 @@
+package me.gokulnair.instagrat.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.gokulnair.instagrat.entity.AppUser;
+import me.gokulnair.instagrat.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public Mono<AppUser> createUser(@RequestParam String name) {
+        return userService.createUser(name);
+    }
+
+    @GetMapping
+    public Flux<AppUser> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
