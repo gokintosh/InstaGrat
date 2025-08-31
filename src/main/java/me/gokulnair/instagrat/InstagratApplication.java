8bae1b9602bb07d1a3248fe1a15fed6111f83b93
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2025-08-31 15:31:18 +0200

    idf

--- src/main/java/me/gokulnair/instagrat/InstagratApplication.java
+++ src/main/java/me/gokulnair/instagrat/InstagratApplication.java
@@ -1,7 +1,9 @@
 package me.gokulnair.instagrat;
 
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InstagratApplication {
@@ -9,5 +11,11 @@ public class InstagratApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InstagratApplication.class, args);
 	}
+
+	@Bean
+	InMemoryChatMemory chatMemory(){
+		return new InMemoryChatMemory();
+	}
+
 
 }
